31775c3aff3f3d19056b98a63e4b66a8cf23367a
Edilson Moizinho <edilsondiasalves@outlook.com>
2021-03-11 01:18:05 -0300

    Adding images from assets

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -5,52 +7,68 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
         child: Container(
-            alignment: Alignment.center,
-            color: Colors.deepOrangeAccent,
-            padding: EdgeInsets.only(top: 30.0, left: 10.0),
-            child: Column(
-              children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    Expanded(
-                        child: Text("Margherita",
-                            textDirection: TextDirection.ltr,
-                            style: TextStyle(
-                                fontSize: 30.0,
-                                decoration: TextDecoration.none,
-                                fontFamily: 'Oxygen',
-                                fontWeight: FontWeight.normal))),
-                    Expanded(
-                        child: Text("Tomato, Mozzarella, Basil",
-                            textDirection: TextDirection.ltr,
-                            style: TextStyle(
-                                fontSize: 20.0,
-                                decoration: TextDecoration.none,
-                                fontFamily: 'Oxygen',
-                                fontWeight: FontWeight.normal))),
-                  ],
-                ),
-                Row(
-                  children: <Widget>[
-                    Expanded(
-                        child: Text("Marinara",
-                            textDirection: TextDirection.ltr,
-                            style: TextStyle(
-                                fontSize: 30.0,
-                                decoration: TextDecoration.none,
-                                fontFamily: 'Oxygen',
-                                fontWeight: FontWeight.normal))),
-                    Expanded(
-                        child: Text("Tomato, Garlic",
-                            textDirection: TextDirection.ltr,
-                            style: TextStyle(
-                                fontSize: 20.0,
-                                decoration: TextDecoration.none,
-                                fontFamily: 'Oxygen',
-                                fontWeight: FontWeight.normal))),
-                  ],
-                )
-              ],
-            )));
+      alignment: Alignment.center,
+      color: Colors.deepOrangeAccent,
+      padding: EdgeInsets.only(top: 30.0, left: 10.0),
+      child: Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Expanded(
+                  child: Text("Margherita",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                          fontSize: 30.0,
+                          decoration: TextDecoration.none,
+                          fontFamily: 'Oxygen',
+                          fontWeight: FontWeight.normal))),
+              Expanded(
+                  child: Text("Tomato, Mozzarella, Basil",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          decoration: TextDecoration.none,
+                          fontFamily: 'Oxygen',
+                          fontWeight: FontWeight.normal))),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                  child: Text("Marinara",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                          fontSize: 30.0,
+                          decoration: TextDecoration.none,
+                          fontFamily: 'Oxygen',
+                          fontWeight: FontWeight.normal))),
+              Expanded(
+                  child: Text("Tomato, Garlic",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          decoration: TextDecoration.none,
+                          fontFamily: 'Oxygen',
+                          fontWeight: FontWeight.normal))),
+            ],
+          ),
+          PizzaImageWidget()
+        ],
+      ),
+    ));
+  }
+}
+
+class PizzaImageWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    AssetImage pizzaAsset = AssetImage('images/pizza1.png');
+    Image pizzaImage = Image(
+      image: pizzaAsset,
+      width: 280.0,
+      height: 280.0,
+    );
+
+    return Container(child: pizzaImage);
   }
 }
