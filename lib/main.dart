869f0e4821b8de87f5f2e43bf7088cc2fff72da8
Edilson Moizinho <edilsondiasalves@outlook.com>
2021-03-11 00:50:03 -0300

    Screen space with rows and columns

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import './ui/home.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     title: "Simple Layouts",
     home: Home(),
   ));
--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -4,19 +4,53 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Container(
-          alignment: Alignment.center,
-          color: Colors.deepOrangeAccent,
-          // width: 192.0,
-          // height: 96.0,
-          // margin: EdgeInsets.all(50.0),
-          child: Text("Pizza",
-              textDirection: TextDirection.ltr,
-              style: TextStyle(
-                  fontSize: 80.0,
-                  decoration: TextDecoration.none,
-                  fontFamily: 'Oxygen',
-                  fontWeight: FontWeight.normal))),
-    );
+        child: Container(
+            alignment: Alignment.center,
+            color: Colors.deepOrangeAccent,
+            padding: EdgeInsets.only(top: 30.0, left: 10.0),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                        child: Text("Margherita",
+                            textDirection: TextDirection.ltr,
+                            style: TextStyle(
+                                fontSize: 30.0,
+                                decoration: TextDecoration.none,
+                                fontFamily: 'Oxygen',
+                                fontWeight: FontWeight.normal))),
+                    Expanded(
+                        child: Text("Tomato, Mozzarella, Basil",
+                            textDirection: TextDirection.ltr,
+                            style: TextStyle(
+                                fontSize: 20.0,
+                                decoration: TextDecoration.none,
+                                fontFamily: 'Oxygen',
+                                fontWeight: FontWeight.normal))),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                        child: Text("Marinara",
+                            textDirection: TextDirection.ltr,
+                            style: TextStyle(
+                                fontSize: 30.0,
+                                decoration: TextDecoration.none,
+                                fontFamily: 'Oxygen',
+                                fontWeight: FontWeight.normal))),
+                    Expanded(
+                        child: Text("Tomato, Garlic",
+                            textDirection: TextDirection.ltr,
+                            style: TextStyle(
+                                fontSize: 20.0,
+                                decoration: TextDecoration.none,
+                                fontFamily: 'Oxygen',
+                                fontWeight: FontWeight.normal))),
+                  ],
+                )
+              ],
+            )));
   }
 }
